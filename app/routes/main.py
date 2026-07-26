c505d94f588c4a84ca0d395e3d1018355d68da77
X-DonTheMafia-X <dikshit0parajuli@gmail.com>
2026-07-26 13:00:09 -0400

    Day 12: Save contact form submissions to the database

--- app/routes/main.py
+++ app/routes/main.py
@@ -1,9 +1,18 @@
 from flask import (
     Blueprint,
-    render_template
+    render_template,
+    redirect,
+    url_for
 )
 
+# Import forms
 from app.forms.contact_form import ContactForm
+
+# Import models
+from app.models.submission import Submission
+
+# Import extensions
+from app.extensions import db
 
 main = Blueprint("main", __name__)
 
@@ -13,7 +22,19 @@ def home():
     form = ContactForm()
 
     if form.validate_on_submit():
-        print("Form validated successfully!")
+        submission = Submission(
+            name=form.name.data,
+            email=form.email.data,
+            message=form.message.data
+        )
+        try:
+            db.session.add(submission)
+            db.session.commit()
+        except Exception:
+            db.session.rollback()
+            raise
+
+        return redirect(url_for("main.home"))
 
     return render_template(
         "pages/home.html",
